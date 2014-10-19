800dd08fbcdbe41885f8d9213747e16c9b715f1e
Jun Yokoyama <jun@larus.org>
2014-10-19 13:16:50 +0900

    VesselRevisionモデルを作成した
    rails g model VesselRevision vessel_id:integer name_ja:string name_en:string type_name:string class_name:string sunk_at:datetime cause_id:integer place_name:sting latitude:decimal longitude:decimal

---
+++ db/migrate/20141019041513_create_vessel_revisions.rb
@@ -0,0 +1,18 @@
+class CreateVesselRevisions < ActiveRecord::Migration
+  def change
+    create_table :vessel_revisions do |t|
+      t.integer :vessel_id, null: true
+      t.string :name_ja, null: false
+      t.string :name_en, null: false
+      t.string :type_name
+      t.datetime :sunk_at, null: true
+      t.string :place_name
+      t.decimal :latitude, precision: 11, scale: 8, null: true
+      t.decimal :longitude, precision: 11, scale: 8, null: true
+      t.integer :classification_id, null: false
+      t.integer :cause_id, null: true
+
+      t.timestamps
+    end
+  end
+end
