ffea97fe43520d306b413d1401d919b907f1da07
Jun Yokoyama <yokoyama_jun_gn@cyberagent.co.jp>
2014-10-23 20:37:16 +0900

    seedデータ及びスクリプトを追加した
    データはテストに使用するもののみ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def seed_data_dir
+  File.join Rails.root, 'db', 'data'
+end
+
+YAML.load_file("#{seed_data_dir}/classifications.yml").each do |mst|
+  s = Classification.find_or_initialize_by(id: mst["id"])
+  s.name_ja = mst["name_ja"]
+  s.name_en = mst["name_en"]
+  s.save!
+end
+
+YAML.load_file("#{seed_data_dir}/causes.yml").each do |mst|
+  s = Cause.find_or_initialize_by(id: mst["id"])
+  s.description = mst["description"]
+  s.save!
+end
+
+YAML.load_file("#{seed_data_dir}/vessels.yml").each do |mst|
+  s = Vessel.find_or_initialize_by(id: mst["id"])
+  s.name_ja = mst["name_ja"]
+  s.name_en = mst["name_en"]
+  s.type_name = mst["type_name"]
+  s.sunk_at  = Time.zone.parse(mst["sunk_at"])
+  s.place_name = mst["place_name"]
+  s.latitude = mst["latitude"]
+  s.longitude = mst["longitude"]
+  s.classification_id = mst["classification_id"]
+  s.cause_id = mst["cause_id"]
+  s.save!
+end
+
