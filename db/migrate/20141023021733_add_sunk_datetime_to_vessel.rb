210c9f782f01c2398199130f5645b21c43923e39
Jun Yokoyama <yokoyama_jun_gn@cyberagent.co.jp>
2014-10-23 18:35:17 +0900

    sunk_month changed.

--- db/migrate/20141023021733_add_sunk_datetime_to_vessel.rb
+++ db/migrate/20141023021733_add_sunk_datetime_to_vessel.rb
@@ -1,8 +1,9 @@
-class AddSunkMonthAndSunkDayAndSunkHourAndSunkMinToVessel < ActiveRecord::Migration
+class AddSunkDatetimeToVessel < ActiveRecord::Migration
   def change
     add_column :vessels, :sunk_month, :integer
     add_column :vessels, :sunk_day, :integer
     add_column :vessels, :sunk_hour, :integer
     add_column :vessels, :sunk_min, :integer
+    add_column :vessels, :sunk_sec, :integer
   end
 end
